2c255c1599e036095440d47e1eb4453bf02342c7
hugo <hugomercalibre@gmail.com>
2020-11-09 21:50:11 +0100

    last deploy

--- server.js
+++ server.js
@@ -13,20 +13,20 @@ const path = require("path")
 const cors = require("cors");
 const app = express()
 
-// mongoose
-//     .connect(process.env.MONGODB_URI, {
-//         keepAlive: true,
-//         useNewUrlParser: true,
-//         useUnifiedTopology: true
-//     })
-//     .then(response => {
-//         console.log(
-//             `Connected to Mongo! Database name: "${response.connections[0].name}"`
-//         );
-//     })
-//     .catch(err => {
-//         console.log("error connecting to mongo", err);
-//     })
+mongoose
+    .connect(process.env.MONGODB_URI, {
+        keepAlive: true,
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(response => {
+        console.log(
+            `Connected to Mongo! Database name: "${response.connections[0].name}"`
+        );
+    })
+    .catch(err => {
+        console.log("error connecting to mongo", err);
+    })
 
 // app.listen(4000, () => {
 //     console.log("app is runing on port 4000");
@@ -45,28 +45,23 @@ app.use(cors({
 app.use(bodyParser.json()) //------> when sending data from the fronteNd and we are using json() we need to parse it, and for that we use the bodyparser.
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
-// app.use(
-//     session({
-//         secret: process.env.SECRET_SESSION,
-//         cookie: {
-//             maxAge: 6000000,
-//         },
-//         resave: false,
-//         saveUninitialized: true,
-//         store: new MongoStore({
-//             mongooseConnection: mongoose.connection,
-//             ttl: 24 * 60 * 60 * 1000, // 1 day
-//         }),
-//     })
-// );
+app.use(
+    session({
+        secret: process.env.SECRET_SESSION,
+        cookie: {
+            maxAge: 6000000,
+        },
+        resave: false,
+        saveUninitialized: true,
+        store: new MongoStore({
+            mongooseConnection: mongoose.connection,
+            ttl: 24 * 60 * 60 * 1000, // 1 day
+        }),
+    })
+);
 
-app.get("/", (req, res, next) => {
-    res.send("its working")
-})
+const auth = require("./routes/auth");
 
-// const auth = require("./routes/auth");
-
-// app.use("/", auth)
+app.use("/", auth)
 
